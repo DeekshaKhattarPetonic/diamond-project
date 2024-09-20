c732cfeda2de28e7140dac736f8aa721764f7fe6
DeekshaKhattarPetonic <deeksha.khattar@petonic.in>
2024-09-20 12:09:12 +0530

    strategies parameters and overall matrix changed

--- src/app/strategies/strategies.component.ts
+++ src/app/strategies/strategies.component.ts
@@ -8,11 +8,20 @@ import { CommonModule } from '@angular/common';
 import {MatTabsModule} from '@angular/material/tabs';
 import { ChartModule } from 'primeng/chart';
 import { CalendarModule } from 'primeng/calendar';
+import { CustomModalComponent } from '../shared/custom-modal/custom-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationModalComponent } from '../shared/confirmation-modal/confirmation-modal.component';
+import { DialogModule } from 'primeng/dialog';
+// import { MessageService } from 'primeng/api';
+// import { ProfileServiceService } from '../../services/profile-service.service';
+import { MatIconModule } from '@angular/material/icon';
+// import ConfirmationModalComponent
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-strategies',
   standalone: true,
-  imports: [CalendarModule, ChartModule, MatTabsModule, MatRadioModule, DropdownModule, FormsModule, CommonModule, ReactiveFormsModule, CheckboxModule],
+  imports: [DialogModule, MatIconModule, DynamicDialogModule, CalendarModule, ChartModule, MatTabsModule, MatRadioModule, DropdownModule, FormsModule, CommonModule, ReactiveFormsModule, CheckboxModule],
   templateUrl: './strategies.component.html',
   styleUrl: './strategies.component.scss'
 })
@@ -32,6 +41,11 @@ export class StrategiesComponent {
   date1: Date | undefined;
 
   date2: Date | undefined;
+
+  constructor(private dialog: MatDialog,) {
+
+  }
+
 
   ngOnInit() {
     this.countries = [
@@ -131,6 +145,18 @@ export class StrategiesComponent {
           }
       }
   };
+}
+
+openModal(){
+  const dialogRef = this.dialog.open(CustomModalComponent, {
+    width: '35vw',
+    data: {
+      head: 'Changes Saved',
+      message: 'The changes have been saved successfully.',
+      buttonTextNo: 'Close',
+    },
+  });
+  this.showTable = false
 }
 
 
